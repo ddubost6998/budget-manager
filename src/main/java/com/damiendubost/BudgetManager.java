6d4230d9de62160c0f2e4747afd506f47d9bc2fa
Damien <ddubost6998@gmail.com>
2024-10-07 15:47:24 +0200

    Initial commit

---
+++ src/main/java/com/damiendubost/BudgetManager.java
@@ -0,0 +1,7 @@
+package com.damiendubost;
+
+public class BudgetManager {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans le Gestionnaire de Budget !");
+    }
+}
